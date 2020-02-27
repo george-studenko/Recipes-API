121907c0e0f62bc515455a0181c303606d06ac92
George Studenko <gnstudenko@gmail.com>
2020-02-28 00:14:56 +0100

    chore: Update chef token

--- test/base_test_case.py
+++ test/base_test_case.py
@@ -16,7 +16,7 @@ class BaseTestCase(TestCase):
     }
 
     def create_app(self):
-        app.config.from_object('infraestructure.config.Heroku_Test_Config')
+        app.config.from_object('infraestructure.config.Test_Config')
         return app
 
     def setUp(self):
